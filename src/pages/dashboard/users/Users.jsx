46bf60762caf6098b96f2b8878852e60a27cebd2
Azamatjon <azamatergasev01@gmail.com>
2025-03-24 21:41:49 +0500

    usersUpdated

--- src/pages/dashboard/users/Users.jsx
+++ src/pages/dashboard/users/Users.jsx
@@ -15,10 +15,10 @@ const Users = () => {
       <h2 className="text-3xl font-semibold mb-6">Users</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {users.map((user) => (
-          <div key={user.id} className="bg-white shadow-md rounded-lg p-5 flex flex-col items-center">
+          <div key={user.id} className="bg-white shadow-md rounded-lg p-20 flex flex-col items-center">
             <img src={user.image} alt={user.firstName} className="w-24 h-24 rounded-full mb-3" />
             <h3 className="text-lg font-semibold">{user.firstName} {user.lastName}</h3>
-            <p className="text-gray-500">{user.email}</p>
+            <p className="text-gray-500 w-10">{user.email}</p>
             <p className="text-gray-600">Age: {user.age}</p>
           </div>
         ))}
